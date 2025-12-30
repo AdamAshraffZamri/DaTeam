41637db1ffa29ad36227b295c2acc8b320a01d0c
Adam Ashraff <adamzamri09@gmail.com>
2025-12-30 09:33:42 +0800

    home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vehicle;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        // Fetch ALL available vehicles for the fleet showcase
+        $vehicles = Vehicle::where('availability', true)
+                            ->orderBy('priceHour', 'asc') // Optional: Order by price or creation
+                            ->get();
+
+        return view('home', compact('vehicles'));
     }
 }
