686dd4e0af905e46690b8a4ffcc48333a3e62a44
Mikael Haqimi <mikaelhaqimii@gmail.com>
2026-04-24 17:47:54 +0800

    fix again

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Notifications\Notification;
 use App\Models\Booking;
 use App\Models\Penalties;
 use App\Models\Payment; 
 use App\Models\Staff;
+use App\Notifications\NewBookingSubmitted;
+use App\Notifications\BookingStatusUpdated;
 
 class FinanceController extends Controller
 {
